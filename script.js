0bb62c1de63d10e73ff00b7feb84dbbc8f3c623f
Dr. Jovo <jonathanckleven@gmail.com>
2026-02-19 12:25:21 -0800

    Sidebar Design
    - Designed a sidebar with semi-functional category buttons
    - Added a cowboy hat - Yeehaw

---
+++ script.js
@@ -0,0 +1,41 @@
+/* Every time a category is clicked, it's children are revealed/hidden */
+document.querySelectorAll(".category_parent").forEach(parent => {
+    parent.addEventListener("click", () => {
+        /* Disable old active parent */
+        try {
+            const old_active = document.getElementById("parent_active");
+            if (old_active != parent ) {
+                old_active.removeAttribute("id");
+                old_active.nextElementSibling.classList.toggle("active");
+            }
+        } catch {
+
+        };
+
+        /* Enable/Disable children items */
+        const siblings = parent.nextElementSibling;
+        siblings.classList.toggle("active");
+        if (siblings.classList.contains("active")) {
+            parent.id = "parent_active";
+        }
+        else {
+            parent.removeAttribute("id");
+        }
+    });
+});
+
+/* Every time an item is clicked, its set to active */
+document.querySelectorAll(".category_item").forEach(item => {
+    item.addEventListener("click", () => {
+        /* Disable old active item */
+        try {
+            const old_active = document.getElementById("item_active");
+            old_active.removeAttribute("id");
+        } catch {
+
+        };
+
+        /* Set to active */
+        item.id = "item_active";
+    });
+});
